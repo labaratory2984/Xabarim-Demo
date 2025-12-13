3e5bc6aa89f5eeb33a2be02129e73878b3eb4c9e
Heisenberg4 <ahmedovnurbek26042005@gmail.com>
2025-12-14 01:13:04 +0500

    Update project

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-
+window.addEventListener("DOMContentLoaded", () => {
+  const parentBox = document.querySelector("body");
+  const successMessage = document.createElement("div");
+  successMessage.classList.add("success__msg");
+  successMessage.innerHTML = `
+          <div class="alert alert-success alert-dismissible fade show" role="alert">
+              <strong>Congratulations 🎉</strong> You logged successfully
+              <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+          </div>
+          `;
+  successMessage.classList.add("alert-position");
+  parentBox.prepend(successMessage);
+  setTimeout(() => {
+    successMessage.remove();
+  }, 3000);
+});
