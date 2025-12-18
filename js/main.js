b57f61b712bd5e6e29272d0864a1dd79a9528176
Heisenberg4 <ahmedovnurbek26042005@gmail.com>
2025-12-18 11:18:47 +0500

    followers are brought from API

--- js/main.js
+++ js/main.js
@@ -16,13 +16,34 @@ window.addEventListener("DOMContentLoaded", () => {
   }, 3000);
   // /Alert success login
   // Followers events:
-  const followers = document.querySelectorAll(".follower");
-  followers.forEach((event) => {
-    event.addEventListener("click", () => {
-      const cardImg = followers.querySelector("img").src;
-      const followerName =
-        followers.querySelector(".profile__name").textContent;
-      const followerStatus = followers.querySelector("span").textContent;
+  const parentFollowers = document.querySelector(".followers");
+  // GET data form API
+  const server = "https://xabarim.pythonanywhere.com/users";
+  fetch(server)
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((item) => {
+        const followerItem = document.createElement("div");
+        followerItem.classList.add("follower-item");
+        followerItem.innerHTML = `
+      <div class="d-flex">
+        <img src = "/img/profile 4.png" class="img-fluid img-follower">
+        <div class="api-data">
+          <p>${item.full_name}</p>
+          <p>${item.username}</p>
+        </div>
+      </div>
+      `;
+        parentFollowers.append(followerItem);
+      });
     });
-  });
+  // const followersClass = document.querySelectorAll(".follower");
+  // followers.forEach((event) => {
+  //   event.addEventListener("click", () => {
+  //     const cardImg = followers.querySelector("img").src;
+  //     const followerName =
+  //       followers.querySelector(".profile__name").textContent;
+  //     const followerStatus = followers.querySelector("span").textContent;
+  //   });
+  // });
 });
