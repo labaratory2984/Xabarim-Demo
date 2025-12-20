4819d8b91fd95a38621836111d2c4fecbede7406
Heisenberg4 <ahmedovnurbek26042005@gmail.com>
2025-12-21 01:40:23 +0500

    user experience on header of messageBox, button styling and creating my profile modal are created today.

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,5 +1,14 @@
 window.addEventListener("DOMContentLoaded", () => {
-  `use strict`;
-  const myProfileBtn = document.querySelector("#my-profile");
-  const modalProfile = document.createElement("div");
+  // `use strict`;
+  const myProfileBtn = document.querySelector(".my-profile"),
+    modalProfile = document.querySelector(".my_profile-modal"),
+    closeMyProfile = document.querySelector("#close-myProfile");
+  myProfileBtn.addEventListener("click", () => {
+    modalProfile.classList.add("show-modal");
+    modalProfile.classList.remove("hide");
+  });
+  closeMyProfile.addEventListener("click", () => {
+    modalProfile.classList.add("hide");
+    modalProfile.classList.remove("show-modal");
+  });
 });
